87beee09d3bbe8e2745d0397b7bf8fe3b4e8a5dd
Rendidotcom <holis112@gmail.com>
2026-01-13 13:35:23 +0700

    v1 pisan

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 // config.js — FINAL
 (function () {
   const API_URL =
-    "https://script.google.com/macros/s/AKfycbwwptTeMkjGOP5tATrGBm9Shw3pBy73QkQQbAOnBh_YzmuLEVWxoI6zwtabN8ce1c2WRg/exec";
+    "https://script.google.com/macros/s/AKfycbx-EoJNCqEqX8X8OSSPFB7N6GF3d5x6ikcLa_XlRHygG21XldMsrwPSvYp2MU8HQZQI0A/exec";
 
   window.API_URL = API_URL;
 })();
